a78217936321aef05c46acb09bba6eaca1f079a9
Luke Mehring <mehringlm@gmail.com>
2020-04-06 03:32:27 -0500

    add RatPack Fridge API

--- src/main/java/luke/mehring/fridge/Main.java
+++ src/main/java/luke/mehring/fridge/Main.java
@@ -1,12 +1,28 @@
 package luke.mehring.fridge;
 
 
-class Main 
-{ 
-    // Your program begins with a call to main(). 
-    // Prints "Hello, World" to the terminal window. 
-    public static void main(String args[]) 
-    { 
-        System.out.println("Hello, World"); 
+import ratpack.server.RatpackServer;
+
+class Main {
+
+    public static void main(String args[]) throws Exception {
+        RatpackServer.start(server -> server.handlers(chain -> chain
+                .prefix("refrigerator", empChain -> { //Fridge Calls
+                    empChain.get("all", ctx -> {  //getAll
+
+                    })
+                    .get(":id", ctx -> { //get single fridge ID
+
+                    })
+                    .post(ctx -> { //create
+
+                    })
+                    .put(ctx -> { //update
+
+                    })
+                    .delete(":id", ctx -> { // Delete fridge by ID
+
+                    });
+                })));
     } 
 } 
