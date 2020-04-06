b46ba3b86e103b4a220b68bf6385d06c4812d6cd
Luke Mehring <mehringlm@gmail.com>
2020-04-06 03:15:59 -0500

    Add SpockTest hellow world

--- src/test/groovy/SpockTest.groovy
+++ src/test/groovy/SpockTest.groovy
@@ -1,5 +1,15 @@
 import spock.lang.Specification
  
 class SpockTest extends Specification {
- 
+
+    def "First Spock Test Temp"() {
+        given:
+
+        when:
+        String a = "Test"
+        String b = "Test2"
+
+        then:
+        assert a != b
+    }
 }
